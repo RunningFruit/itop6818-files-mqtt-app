a5e5841eb18338802fc9897a1536f0e15894e9ce
brightereyer2 <brightereyer2@163.com>
2020-03-04 14:24:35 +0800

    *

--- c-programs/buzzer/mqttclient.c
+++ c-programs/buzzer/mqttclient.c
@@ -37,7 +37,7 @@ void my_message_callback(struct mosquitto *mosq, void *userdata, const struct mo
 		/**************************
 		 *      json的解析
 		 **************************/
-		char jsonString[];
+		char jsonString[1024];
 		strcpy(jsonString, (char *)message->payload);
 		cJSON *json = 0, *json_type = 0, *json_cmd = 0;
 		json = cJSON_Parse(jsonString);
@@ -52,17 +52,17 @@ void my_message_callback(struct mosquitto *mosq, void *userdata, const struct mo
 			}
 			json_cmd = cJSON_GetObjectItem(json, "cmd");
 			//如果类型是 字符串
-			if (json_cmd->cmd == cJSON_String) {
-				printf("指令：%s\n", json_name->valuestring);
+			if (json_cmd->type == cJSON_String) {
+				printf("指令：%s\n", json_cmd->valuestring);
 			}
 			//释放内存
 			cJSON_Delete(json);
 		}
 
 		//判断指令
-		if (strcmp(json_type, "led") == 0) {
+		if (strcmp(json_type->valuestring, "led") == 0) {
 
-			if (strcmp(json_cmd, "on") == 0) {
+			if (strcmp(json_cmd->valuestring, "on") == 0) {
 				if ((fd = open(buzzer, O_RDWR | O_NOCTTY | O_NDELAY)) < 0)
 					printf("open %s failed\n", buzzer);
 				else
@@ -87,7 +87,7 @@ void my_message_callback(struct mosquitto *mosq, void *userdata, const struct mo
 
 void my_connect_callback(struct mosquitto *mosq, void *userdata, int result)
 {
-	int i;
+	// int i;
 	if (!result) {
 		/* Subscribe to broker information topics on successful connect. */
 		mosquitto_subscribe(mosq, NULL, topic[1], 2);
@@ -112,54 +112,3 @@ void my_log_callback(struct mosquitto *mosq, void *userdata, int level, const ch
 	printf("%s\n", str);
 }
 
-
-struct mosquitto *mosq = NULL;
-char buff[MSG_MAX_SIZE];
-
-void init_mqtt()
-{
-
-	//libmosquitto 库初始化
-	mosquitto_lib_init();
-	//创建mosquitto客户端
-	mosq = mosquitto_new(NULL, session, NULL);
-	if (!mosq) {
-		printf("create client failed..\n");
-		mosquitto_lib_cleanup();
-		return;
-	}
-	//设置回调函数，需要时可使用
-	mosquitto_log_callback_set(mosq, my_log_callback);
-	mosquitto_connect_callback_set(mosq, my_connect_callback);
-	mosquitto_message_callback_set(mosq, my_message_callback);
-	mosquitto_subscribe_callback_set(mosq, my_subscribe_callback);
-
-
-	//连接服务器
-	if (mosquitto_connect(mosq, HOST, PORT, KEEP_ALIVE)) {
-		fprintf(stderr, "Unable to connect.\n");
-		return;
-	}
-	//开启一个线程，在线程里不停的调用 mosquitto_loop() 来处理网络信息
-	int loop = mosquitto_loop_start(mosq);
-	if (loop != MOSQ_ERR_SUCCESS)
-	{
-		printf("mosquitto loop error\n");
-		return;
-	}
-
-}
-
-void pub_mqtt() {
-	while (fgets(buff, MSG_MAX_SIZE, stdin) != NULL)
-	{
-		//发布消息
-		mosquitto_publish(mosq, NULL, topic[2], strlen(buff) + 1, buff, 0, 0);
-		memset(buff, 0, sizeof(buff));
-	}
-}
-
-void end_mqtt() {
-	mosquitto_destroy(mosq);
-	mosquitto_lib_cleanup();
-}
--- c-programs/buzzer/mqttclient.h
+++ c-programs/buzzer/mqttclient.h
@@ -10,8 +10,6 @@ void my_message_callback(struct mosquitto *mosq, void *userdata, const struct mo
 void my_connect_callback(struct mosquitto *mosq, void *userdata, int result);
 void my_subscribe_callback(struct mosquitto *mosq, void *userdata, int mid, int qos_count, const int *granted_qos);
 void my_log_callback(struct mosquitto *mosq, void *userdata, int level, const char *str);
-void init_mqtt();
-void pub_mqtt();
-void end_mqtt();
+
 
 #endif
